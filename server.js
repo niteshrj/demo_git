2eadb6a0c9e52fcc43e7afc216b93c1750dad4a6
Nitesh Ranjan <niteshr@INniteshr.local>
2018-06-23 13:10:34 +0530

    Added index.html and server.js file

---
+++ server.js
@@ -0,0 +1,18 @@
+const http = require('http');
+// const fs = require('fs');
+const express = require('express');
+
+// function app(req,res){
+//   const data = fs.readFileSync('./index.html');
+//   console.log(req.method);
+//   if(req.url == '/')
+//     res.write(data);
+//   else
+//     res.write('Not available');
+//   res.end();
+// }
+
+let app = express();
+app.use(express.static('./'));
+const server = http.createServer(app);
+server.listen(8000, ()=> console.log("Server is listening at 8000"));
